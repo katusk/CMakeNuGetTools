9f3618bd003095d510f4eb03c70d12444bfed708
Kristof Katus <kristof.katus@gmail.com>
2020-03-21 21:56:21 +0100

    Clean up afterwards nuget_dependencies macro

--- cmake/NuGetImport.cmake
+++ cmake/NuGetImport.cmake
@@ -15,14 +15,24 @@ macro(nuget_dependencies)
         return()
     endif()
     # Process each PACKAGE argument pack one-by-one
-    set(ARGS_HEAD "")
-    set(ARGS_TAIL ${ARGV})
-    while(NOT "${ARGS_TAIL}" STREQUAL "")
-        _nuget_helper_cut_arg_list(PACKAGE "${ARGS_TAIL}" ARGS_HEAD ARGS_TAIL)
-        _nuget_single_dependencies(${ARGS_HEAD})
-        list(FIND ARGS_HEAD PACKAGE PACKAGE_IDX)
-        math(EXPR PACKAGE_ID_IDX "${PACKAGE_IDX} + 1")
-        list(GET ARGS_HEAD ${PACKAGE_ID_IDX} PACKAGE_ID)
-        _nuget_core_import_cmake_exports_set_cmake_paths("${PACKAGE_ID}")
+    set(nuget_dependencies_ARGS_HEAD "")
+    set(nuget_dependencies_ARGS_TAIL ${ARGV})
+    while(NOT "${nuget_dependencies_ARGS_TAIL}" STREQUAL "")
+        _nuget_helper_cut_arg_list(
+            PACKAGE
+            "${nuget_dependencies_ARGS_TAIL}"
+            nuget_dependencies_ARGS_HEAD
+            nuget_dependencies_ARGS_TAIL
+        )
+        _nuget_single_dependencies(${nuget_dependencies_ARGS_HEAD})
+        list(FIND nuget_dependencies_ARGS_HEAD PACKAGE nuget_dependecies_PACKAGE_IDX)
+        math(EXPR nuget_dependecies_PACKAGE_ID_IDX "${nuget_dependecies_PACKAGE_IDX} + 1")
+        list(GET nuget_dependencies_ARGS_HEAD ${nuget_dependecies_PACKAGE_ID_IDX} nuget_dependecies_PACKAGE_ID)
+        _nuget_core_import_cmake_exports_set_cmake_paths("${nuget_dependecies_PACKAGE_ID}")
     endwhile()
+    unset(nuget_dependencies_ARGS_HEAD)
+    unset(nuget_dependencies_ARGS_TAIL)
+    unset(nuget_dependecies_PACKAGE_IDX)
+    unset(nuget_dependecies_PACKAGE_ID_IDX)
+    unset(nuget_dependecies_PACKAGE_ID)
 endmacro()
