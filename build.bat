953e96fa9ba79e55fd6638b90ee201e9e76517ff
Kristof Katus <kristof.katus@gmail.com>
2020-04-04 17:24:24 +0200

    Run all current tests

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 if not exist "build/meta/x64-windows" (mkdir build\meta\x64-windows)
 cd build\meta\x64-windows
 cmake -G "Visual Studio 15 2017 Win64" ..\..\..\
-ctest -C Release
+cmake --build . --config Release
+ctest -C Release --verbose
