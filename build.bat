95b7c597a9ef4bb485305649626d62666ced59b5
Kristof Katus <kristof.katus@gmail.com>
2020-04-05 23:05:53 +0200

    Converted build.bat to build.cmake

--- build.bat
+++
@@ -1,5 +0,0 @@
-if not exist "build/meta/x64-windows" (mkdir build\meta\x64-windows)
-cd build\meta\x64-windows
-cmake -G "Visual Studio 15 2017 Win64" ..\..\..\
-cmake --build . --config Release
-ctest -C Release --verbose
---
+++ build.cmake
@@ -0,0 +1,14 @@
+# Set working directory
+set(WORKDIR "${CMAKE_CURRENT_LIST_DIR}/build/meta/x64-windows")
+execute_process(COMMAND "${CMAKE_COMMAND}" -E make_directory "${WORKDIR}")
+
+# Run tests
+execute_process(COMMAND "${CMAKE_COMMAND}" -G "Visual Studio 15 2017 Win64" "${CMAKE_CURRENT_LIST_DIR}"
+    WORKING_DIRECTORY "${WORKDIR}"
+)
+execute_process(COMMAND "${CMAKE_COMMAND}" --build . --config Release
+    WORKING_DIRECTORY "${WORKDIR}"
+)
+execute_process(COMMAND "${CMAKE_CTEST_COMMAND}" -C Release --verbose
+    WORKING_DIRECTORY "${WORKDIR}"
+)
