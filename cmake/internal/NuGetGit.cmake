2504e820bfc1ada5d8a2845d8c1d51e1f4a6c790
Kristof Katus <kristof.katus@gmail.com>
2020-04-21 21:27:09 +0200

    remove unnecessary checks after parsing git tags and semver

--- cmake/internal/NuGetGit.cmake
+++ cmake/internal/NuGetGit.cmake
@@ -14,14 +14,13 @@ function(_nuget_git_parse_git_describe
     # NOTE: consider using "--first-parent"; see:
     # https://git-scm.com/docs/git-describe#Documentation/git-describe.txt---first-parent
     execute_process(
-        COMMAND ${GIT_EXECUTABLE} describe --tags --long --match "${GIT_TAG_PREFIX}*"
-        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-        OUTPUT_STRIP_TRAILING_WHITESPACE
+        COMMAND "${GIT_EXECUTABLE}" describe --tags --long --match "${GIT_TAG_PREFIX}*"
+        WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
         OUTPUT_VARIABLE GIT_DESCRIBE_OUTPUT
         ERROR_VARIABLE GIT_DESCRIBE_ERROR_VAR
         RESULT_VARIABLE GIT_DESCRIBE_RESULT_VAR
+        OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-    string(STRIP "${GIT_DESCRIBE_OUTPUT}" GIT_DESCRIBE_OUTPUT)
     _nuget_helper_error_if_not_empty("${GIT_DESCRIBE_ERROR_VAR}" "Running Git describe encountered some errors: ")
     if(NOT ${GIT_DESCRIBE_RESULT_VAR} EQUAL 0)
         message(FATAL_ERROR "Git describe returned with: \"${GIT_DESCRIBE_RESULT_VAR}\"")
@@ -33,17 +32,10 @@ function(_nuget_git_parse_git_describe
     string(REGEX MATCH "${REGEX_GIT_DESCRIBE}" MATCH_GIT_DESCRIBE "${GIT_DESCRIBE_OUTPUT}")
     _nuget_helper_error_if_empty("${MATCH_GIT_DESCRIBE}" "Cannot match \"${GIT_DESCRIBE_OUTPUT}\" with \"${REGEX_GIT_DESCRIBE}\": ")
     string(REGEX REPLACE "${REGEX_GIT_DESCRIBE}" "\\1" TAG_WITHOUT_PREFIX "${GIT_DESCRIBE_OUTPUT}")
-    _nuget_helper_error_if_empty("${TAG_WITHOUT_PREFIX}" "Cannot parse tag part of Git describe's output: ")
     # Due to "--long" in the above below is also emitted even if most recent commit has the tag
     # (COMMITS_SINCE_MOST_RECENT_TAG is going to be 0 if that is the case).
     string(REGEX REPLACE "${REGEX_GIT_DESCRIBE}" "\\2" COMMITS_SINCE_MOST_RECENT_TAG "${GIT_DESCRIBE_OUTPUT}")
-    _nuget_helper_error_if_empty("${COMMITS_SINCE_MOST_RECENT_TAG}"
-        "Cannot parse number of commits since most recent tag part of Git describe's output: "
-    )
     string(REGEX REPLACE "${REGEX_GIT_DESCRIBE}" "\\3" MOST_RECENT_COMMIT_ABBREV "${GIT_DESCRIBE_OUTPUT}")
-    _nuget_helper_error_if_empty("${MOST_RECENT_COMMIT_ABBREV}"
-        "Cannot parse most recent abbreviated commit part of Git describe's output: "
-    )
     set(${TAG_WITHOUT_PREFIX_OUT} "${TAG_WITHOUT_PREFIX}" PARENT_SCOPE)
     set(${COMMITS_SINCE_MOST_RECENT_TAG_OUT} "${COMMITS_SINCE_MOST_RECENT_TAG}" PARENT_SCOPE)
     set(${MOST_RECENT_COMMIT_ABBREV_OUT} "${MOST_RECENT_COMMIT_ABBREV}" PARENT_SCOPE)
@@ -56,18 +48,17 @@ function(nuget_git_get_current_branch_name BRANCH_NAME_OUT)
         message(FATAL_ERROR "Git was not found: cannot get name of current branch.")
     endif()
     execute_process(
-        COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
-        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-        OUTPUT_STRIP_TRAILING_WHITESPACE
+        COMMAND "${GIT_EXECUTABLE}" rev-parse --abbrev-ref HEAD
+        WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
         OUTPUT_VARIABLE GIT_BRANCH_OUTPUT
         ERROR_VARIABLE GIT_BRANCH_ERROR_VAR
         RESULT_VARIABLE GIT_BRANCH_RESULT_VAR
+        OUTPUT_STRIP_TRAILING_WHITESPACE
     )
     _nuget_helper_error_if_not_empty("${GIT_BRANCH_ERROR_VAR}" "Running Git rev-parse --abbrev-ref HEAD encountered some errors: ")
     if(NOT ${GIT_BRANCH_RESULT_VAR} EQUAL 0)
         message(FATAL_ERROR "Git rev-parse --abbrev-ref HEAD returned with: \"${GIT_BRANCH_RESULT_VAR}\"")
     endif()
-    string(STRIP "${GIT_BRANCH_OUTPUT}" GIT_BRANCH_OUTPUT)
     set(${BRANCH_NAME_OUT} "${GIT_BRANCH_OUTPUT}" PARENT_SCOPE)
 endfunction()
 
@@ -78,18 +69,17 @@ function(nuget_git_get_current_commit_sha1 HEAD_COMMIT_SHA1_OUT)
         message(FATAL_ERROR "Git was not found: cannot get SHA-1 of HEAD.")
     endif()
     execute_process(
-        COMMAND ${GIT_EXECUTABLE} rev-parse --verify HEAD
-        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-        OUTPUT_STRIP_TRAILING_WHITESPACE
+        COMMAND "${GIT_EXECUTABLE}" rev-parse --verify HEAD
+        WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
         OUTPUT_VARIABLE GIT_HEAD_COMMIT_OUTPUT
         ERROR_VARIABLE GIT_HEAD_COMMIT_ERROR_VAR
         RESULT_VARIABLE GIT_HEAD_COMMIT_RESULT_VAR
+        OUTPUT_STRIP_TRAILING_WHITESPACE
     )
     _nuget_helper_error_if_not_empty("${GIT_HEAD_COMMIT_ERROR_VAR}" "Running Git rev-parse --verify HEAD encountered some errors: ")
     if(NOT ${GIT_HEAD_COMMIT_RESULT_VAR} EQUAL 0)
         message(FATAL_ERROR "Git rev-parse --verify HEAD returned with: \"${GIT_HEAD_COMMIT_RESULT_VAR}\"")
     endif()
-    string(STRIP "${GIT_HEAD_COMMIT_OUTPUT}" GIT_HEAD_COMMIT_OUTPUT)
     set(${HEAD_COMMIT_SHA1_OUT} "${GIT_HEAD_COMMIT_OUTPUT}" PARENT_SCOPE)
 endfunction()
 
@@ -100,17 +90,16 @@ function(nuget_git_get_remote_url REMOTE_URL_OUT)
         message(FATAL_ERROR "Git was not found: cannot get URL of remote.")
     endif()
     execute_process(
-        COMMAND ${GIT_EXECUTABLE} ls-remote --get-url
-        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-        OUTPUT_STRIP_TRAILING_WHITESPACE
+        COMMAND "${GIT_EXECUTABLE}" ls-remote --get-url
+        WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
         OUTPUT_VARIABLE GIT_REMOTE_URL_OUTPUT
         ERROR_VARIABLE GIT_REMOTE_URL_ERROR_VAR
         RESULT_VARIABLE GIT_REMOTE_URL_RESULT_VAR
+        OUTPUT_STRIP_TRAILING_WHITESPACE
     )
     _nuget_helper_error_if_not_empty("${GIT_REMOTE_URL_ERROR_VAR}" "Running Git ls-remote --get-url encountered some errors: ")
     if(NOT ${GIT_REMOTE_URL_RESULT_VAR} EQUAL 0)
         message(FATAL_ERROR "Git ls-remote --get-url returned with: \"${GIT_REMOTE_URL_RESULT_VAR}\"")
     endif()
-    string(STRIP "${GIT_REMOTE_URL_OUTPUT}" GIT_REMOTE_URL_OUTPUT)
     set(${REMOTE_URL_OUT} "${GIT_REMOTE_URL_OUTPUT}" PARENT_SCOPE)
 endfunction()
